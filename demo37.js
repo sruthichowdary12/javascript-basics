356743815e3b9fe0d36f18c7444b92d725c7ad89
Sruthi chowdary Anumolu <Sruthichowdaryanumolu@gmail.com>
2024-05-14 09:21:41 +0530

    Add files via upload

---
+++ demo37.js
@@ -0,0 +1,17 @@
+let n = parseInt(prompt("Enter Input",0))
+let digit=0
+let sum=0
+let temp=n
+while(n!=0)
+{
+    digit = parseInt(n%10)
+    sum = sum+Math.pow(digit,3)
+    n=parseInt(n/10)
+}
+if(sum==temp)
+{
+    document.writeln(`${temp} is an armstrong number`)
+}
+else{
+    document.writeln(`${temp} is not armstrong number`)
+}
